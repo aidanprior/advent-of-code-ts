3cb52dba5f28c60b647090ab986e4a5a00f089e0
aidanprior <priorlyaidan@yahoo.com>
2024-12-05 15:55:40 -0500

    solution(day5.ts): B

--- solutions/day5.ts
+++ solutions/day5.ts
@@ -41,3 +41,55 @@ export function A({ antiRules, updates }: parsedInput, debug = false): number {
     .reduce((sum, update) => sum + update[Math.floor(update.length / 2)], 0);
   return sum;
 }
+
+function fixUpdate(
+  antiRules: Rules,
+  update: Update,
+  debug: boolean
+): Update | boolean {
+  if (debug) console.log('\n', logI++, update, '\n');
+  let errorIndicies: Record<number, number> = {};
+  let madeEdit = false;
+  for (let i = 0; i < update.length; i++) {
+    if (debug)
+      console.log(
+        `${i}\t${update.map((n, idx) =>
+          idx === i ? `[${n}]` : ` ${n} `
+        )}`.replaceAll(',', ' ')
+      );
+    if (errorIndicies[update[i]] !== undefined) {
+      madeEdit = true;
+      const errorIdx = errorIndicies[update[i]];
+      if (debug)
+        console.log(
+          `\t${update.map((n, idx) =>
+            idx === i ? `[${n}]` : idx === errorIdx ? `(${n})` : ` ${n} `
+          )}`.replaceAll(',', ' ')
+        );
+      const temp = update[i];
+      update[i] = update[errorIdx];
+      update[errorIdx] = temp;
+      errorIndicies = {};
+      i = 0;
+    }
+    antiRules[update[i]]?.forEach((page) => {
+      errorIndicies[page] = i;
+    });
+  }
+  if (debug)
+    if (madeEdit) console.log(`Edited: ${update}`);
+    else console.log('Correctly Ordered');
+  return madeEdit ? update : false;
+}
+
+export function B({ antiRules, updates }: parsedInput, debug = false): number {
+  if (debug) console.table(antiRules);
+  const sum = updates
+    .map((update) => fixUpdate(antiRules, update, debug))
+    .filter((update) => Array.isArray(update))
+    .reduce(
+      (sum, update: Update) => sum + update[Math.floor(update.length / 2)],
+      0
+    );
+  return sum;
+}
